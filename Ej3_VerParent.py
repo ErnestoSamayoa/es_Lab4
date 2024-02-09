99499d30f561d0c90b4bbe7d1d1d22e6fb520ee3
ErnestoSamayoa <esamayoaj1@miumg.edu.gt>
2024-02-09 11:18:56 -0600

    Ejercicio#4.3

---
+++ Ej3_VerParent.py
@@ -0,0 +1,19 @@
+#3. Verificación de Paréntesis Balanceados:
+
+def parentesis_balancedos(cadena):
+    pila = []
+    for caracter in cadena:
+        if caracter == '(':
+            pila.append(caracter)
+        elif caracter == ')':
+            if not pila or pila.pop() != '(':
+                return False
+    return not pila, cadena
+
+cadena = "(3, 4, 55, 4, 3, 3)"
+resultado, cadena = parentesis_balancedos(cadena)
+
+if resultado:
+    print("Los paréntesis en la cadena", cadena, "están balanceados.")
+else:
+    print("Los paréntesis en la cadena", cadena, "no están balanceados.") 
